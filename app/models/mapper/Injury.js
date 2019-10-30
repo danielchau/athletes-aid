0f1113be8ea6505c9fdd4e4bd818cf0515d5bc50
chrisneuf <neufeld.christian1@gmail.com>
2019-10-29 18:32:02 -0700

    Initial dynamoDB templates

---
+++ app/models/mapper/Injury.js
@@ -0,0 +1,26 @@
+const { DynamoDbSchema, DynamoDbTable } = require("@aws/dynamodb-data-mapper");
+const { v4 } = require("uuid");
+
+class InjuryReports {}
+
+Object.defineProperties(User.prototype, {
+  [DynamoDbTable]: {
+    value: "InjuryReports"
+  },
+  [DynamoDbSchema]: {
+    value: {
+      id: {
+        type: "String",
+        keyType: "HASH",
+        defaultProvider: v4
+      },
+      createdAt: {
+        type: "Date",
+        keyType: "RANGE"
+      },
+      description: { type: "String"}
+    }
+  }
+});
+
+module.exports = {InjuryReports};
