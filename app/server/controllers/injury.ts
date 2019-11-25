2916789eb223e5f43ee9754642e61667cec6b1eb
Christian Neufeld <neufeld.christian1@gmail.com>
2019-11-24 16:33:26 -0800

    api call works to post injury in dynamo (#14)
    * api call works to post injury in dynamo
    
    * Get injuries in date range

---
+++ app/server/controllers/injury.ts
@@ -0,0 +1,74 @@
+import { Request, Response } from "express";
+import { Injury } from "../models/schema/Injury";
+
+import { Logger } from "@overnightjs/logger";
+
+import * as injuryModel from "../models/injury";
+
+export const postInjury = async (req: Request, res: Response) => {
+  Logger.Info(req);
+  try {
+    const injury = Object.assign(new Injury(), {
+      createdBy: req.body.createdBy,
+
+      active: req.body.active,
+
+      teamName: req.body.teamName,
+
+      athleteName: req.body.athleteName,
+
+      injuryDate: req.body.injuryDate,
+
+      isSportsRelated: req.body.isSportsRelated,
+
+      eventType: req.body.eventType,
+
+      position: req.body.position,
+
+      sideOfBody: req.body.sideOfBody,
+
+      locationOnBody: req.body.locationOnBody,
+
+      injuryType: req.body.injuryType,
+
+      severity: req.body.severity,
+
+      status: req.body.status,
+
+      mechanism: req.body.mechanism,
+
+      injuryDescription: req.body.injuryDescription
+    });
+
+    let id: string = await injuryModel.putInjury(injury);
+
+    let response = {
+      message: "Injury Created",
+      data: {
+        injuryId: id
+      }
+    };
+    res.json(response);
+  } catch (e) {
+    Logger.Info(e);
+    return res.status(500).send("Failed to create injury");
+  }
+};
+
+export const getInjuriesByRange = async (req: Request, res: Response) => {
+  try {  
+    let injuries = new Array<Injury>();
+    injuries = await injuryModel.getInjuriesByRange(req.query.startDate, req.query.endDate, req.query.teamName);
+
+    let response = {
+      message: "Injuries By date and team",
+      data: {
+        injuries
+      }
+    };
+    res.json(response);
+  } catch (e) {
+    Logger.Info(e);
+    return res.status(500).send("Failed to get injuries in range");
+  }
+};
--- app/server/models/injury.ts
+++ app/server/models/injury.ts
@@ -1,5 +1,12 @@
 import { Injury } from "./schema/Injury";
+import {
+  between,
+  ConditionExpression,
+  ConditionExpressionPredicate
+} from "@aws/dynamodb-expressions";
 import mapper from "./mapper";
+import { Logger } from "@overnightjs/logger";
+import { loggerModeArr } from "@overnightjs/logger/lib/constants";
 
 /**
  * Create a Injury in DynamoDb
@@ -9,20 +16,39 @@ import mapper from "./mapper";
  * @param {string} description a descrition of the injury
  * @return {Promise} A promise which contains the id of the user requested
  */
-export async function putInjury(
-  createdBy: string,
-  athlete: string,
-  description: string
-): Promise<string> {
-  const injury = Object.assign(new Injury(), {
-    createdAt: new Date(),
-    createdBy: createdBy,
-    athlete: athlete,
-    description: description
-  });
+export async function putInjury(injury: Injury): Promise<string> {
   return mapper.put(injury).then((data: Injury) => {
     return data.id;
   });
+}
+
+export async function getInjuriesByRange(
+  startDate: string,
+  endDate: string,
+  teamName: string
+): Promise<Array<Injury>> {
+  let startTime = new Date(startDate).getTime() / 1000;
+  let endTime = new Date(endDate).getTime() / 1000;
+
+  let injuries = new Array<Injury>();
+
+  const equalsExpressionPredicate = between(startTime, endTime);
+  const equalsExpression: ConditionExpression = {
+    ...equalsExpressionPredicate,
+    subject: "injuryDate"
+  };
+
+  Logger.Info(equalsExpression);
+
+  for await (const entry of mapper.query(
+    Injury,
+    { teamName: teamName },
+    { filter: equalsExpression, indexName: "teamName-index" }
+  )) {
+    injuries.push(entry);
+  }
+
+  return injuries;
 }
 
 /**
@@ -31,21 +57,22 @@ export async function putInjury(
  * @param {string} athlete the id of the user
  * @return {Injury} An Injury object containing the Injury Data
  */
-export async function getInjury(athlete: string): Promise<Injury> {
-  let injury = null;
-  for await (const entry of mapper.query(
-    Injury,
-    { athlete: athlete },
-    { indexName: "athlete-index" }
-  )) {
-    injury = entry;
-  }
+//TODO
+// export async function getInjury(athlete: string): Promise<Injury> {
+//   let injury = null;
+//   for await (const entry of mapper.query(
+//     Injury,
+//     { athlete: athlete },
+//     { indexName: "athlete-index" }
+//   )) {
+//     injury = entry;
+//   }
 
-  if (injury) {
-    console.log(injury);
-    return injury;
-  } else {
-    console.log("Didnt find Injury report for athlete");
-    // Todo: return something
-  }
-}
+//   if (injury) {
+//     console.log(injury);
+//     return injury;
+//   } else {
+//     console.log("Didnt find Injury report for athlete");
+//     // Todo: return something
+//   }
+// }
