1c0644353dfa9456f27fac90f21ea0a08b4c90ae
Arjan Sandhu <arjan.97@hotmail.com>
2020-01-25 15:21:23 -0800

    more athlete endpoints (#24)

--- app/server/models/Athlete.ts
+++ app/server/models/Athlete.ts
@@ -1,19 +1,25 @@
 import { Athlete } from "./schema/Athlete";
-import {
-  between,
-  ConditionExpression,
-  ConditionExpressionPredicate
-} from "@aws/dynamodb-expressions";
 import mapper from "./mapper";
-import { Logger } from "@overnightjs/logger";
-import { loggerModeArr } from "@overnightjs/logger/lib/constants";
-
 
 export async function putAthlete(athlete: Athlete): Promise<string> {
   return mapper.put(athlete).then((data: Athlete) => {
     return data.id;
   });
-
-
 }
 
+export async function getAthlete(athleteId: string): Promise<Athlete> {
+  return mapper
+    .get(Object.assign(new Athlete(), { id: athleteId }))
+    .then((athlete: Athlete) => {
+      console.log(athlete);
+      return athlete;
+    });
+}
+
+export async function getAllAthletes(): Promise<Array<Athlete>> {
+  let athletes = new Array<Athlete>();
+  for await (const entry of mapper.scan(Athlete)) {
+    athletes.push(entry);
+  }
+  return athletes;
+}
