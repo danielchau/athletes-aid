487d9a5a2066b491d8779d77771fec2d39ce3226
danielchau <d.chau@alumni.ubc.ca>
2020-03-09 19:09:13 -0700

    Add inactive injury switch to whole page

--- app/src/components/BodyVisualization.tsx
+++ app/src/components/BodyVisualization.tsx
@@ -3,7 +3,7 @@ import { Injury } from "../util/types";
 import { bodyVisualizationStyles } from "../styles/react/BodyVisualizationStyles";
 // @ts-ignore
 import Body from "../util/body.jpg";
-import { Grid, Typography, Divider, Switch, FormControlLabel } from "@material-ui/core";
+import { Grid, Typography, Divider } from "@material-ui/core";
 
 interface BodyVisualizationProps {
     injuries: Injury[];
@@ -21,15 +21,6 @@ export default function BodyVisualization(props: BodyVisualizationProps) {
             setInjuries(props.injuries.filter(i => i.active));
         }
     }, [props.injuries]);
-
-    const onSwitch = (_: any, checked: boolean) => {
-        setShowInactive(checked);
-        if (checked) {
-            setInjuries(props.injuries);
-        } else {
-            setInjuries(props.injuries.filter(i => i.active));
-        }
-    };
 
     const getStyles = (injury: Injury | string, isLegend: boolean) => {
         let color = "";
@@ -206,10 +197,6 @@ export default function BodyVisualization(props: BodyVisualizationProps) {
                     <Typography variant="h6">Injuries Breakdown</Typography>
                     <Divider light style={{ marginBottom: "8px" }} />
                     <div className={classes.legendItemContainer}>{getLegend()}</div>
-                    <FormControlLabel
-                        control={<Switch checked={showInactive} onChange={onSwitch} />}
-                        label="Show Inactive Injuries"
-                    />
                 </Grid>
             </Grid>
         </div>
