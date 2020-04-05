64293ce9af5b2bf9a92c0b796634736a0619e032
Marshall Elford <marshall.elford@aimsio.com>
2020-04-05 00:39:10 -0700

    Backend test working!

---
+++ app/tests/backend/user.test.ts
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { mocked } from 'ts-jest/utils';
+import { putUser, getUser } from '../../server/models/user'
+import mapper from "../../server/models/mapper";
+
+jest.mock('../../server/models/mapper');
+mapper.put.mockResolvedValue({name: "Test Name", email: "Test Email", userType: "Test userType", id: "Test ID"});
+
+describe('user test', () => {
+    test('putUser', async () => {        
+        const personID = await putUser({name: "Test Name", email: "Test Email", userType: "Test userType"});
+        
+        expect(personID).toBeDefined();
+        expect(personID).toBe('Test ID');
+    });
+    
+    test('getUser', async () => {
+        const person = await getUser("Test Email");
+        
+        expect(person).toBeDefined();
+    });
+});
